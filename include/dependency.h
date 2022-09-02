497de135cc37495352ffc8840eaa0244d1e93a20
Sunil Sharma P <sunilsharma.pv@gmail.com>
2022-09-02 18:28:16 +0530

    Initial commit
    Basic library sources and includes added

---
+++ include/dependency.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#include <stdio.h>
+#include "extern.h"
+
+/* It is a shared object avilable in libshared.so */
+LIB_EXTERN int dependency_func();
---
+++ src/dependency.c
@@ -0,0 +1,7 @@
+#include "include/dependency.h"
+
+int dependency_func()
+{
+    printf("This print is coming from the Dependency\n");
+    return 0;
+}
